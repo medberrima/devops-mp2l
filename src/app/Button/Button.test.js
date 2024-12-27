8ceb189d33c7d0ca583510e2e47dce9ae3bc15c6
Med Berrima <mberrima@queney.com>
2024-12-27 22:12:45 +0100

    remove unused  fireEvent import

--- src/app/Button/Button.test.js
+++ src/app/Button/Button.test.js
@@ -1,7 +1,8 @@
-import '@testing-library/jest-dom';  
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+// import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Button from './Button'; 
+import Button from './Button';
 
 describe('Button Component', () => {
   test('renders the button with correct label', () => {
